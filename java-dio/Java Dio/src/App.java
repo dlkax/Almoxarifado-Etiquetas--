553bb0d8c78a3f28767fc246aa5c2dd16e01f023
Diego Buzo <90202864+dlkax@users.noreply.github.com>
2026-01-17 21:09:55 +0000

    aaaa

--- java-dio/Java Dio/src/App.java
+++
@@ -1,5 +0,0 @@
-public class App {
-    public static void main(String[] args){
-        System.out.println("hello, my name is Diego, and i study coding at wizard, i'am 19y, i live in Salto/SP and i like to play with my friends on weekends, studing with my brother, oh, sorry. I study english at wizard all saturdays. ");
-    }
-}
